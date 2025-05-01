5b03f0ad9701e0991996fb21f7ebfd4ce9943b8f
Nina-Watson-ADA <nina.watson@ada.ac.uk>
2025-05-01 10:50:32 +0000

    finished the task

--- dictionary.py
+++ dictionary.py
@@ -20,7 +20,6 @@ library = [
 def loan_status(lib):
     for book in lib:
         book_info = f"{book['title']} by {book['author']}"
-
         if book["isLoaned"]:
             print(f"Out on loan: {book_info}")
         else:
@@ -29,33 +28,44 @@ def loan_status(lib):
 
 def get_books_by_author(library, author_name):
     books_by_author = []
-
     for book in library:
         if book["author"] == author_name:
             book_status = "Out on loan" if book["isLoaned"] else "On the shelf"
             books_by_author.append(f"{book['title']} - {book_status}")
-
     return books_by_author
 
-
 def search_by_book_name(library, search_term):
-    pass
+    return [book for book in library if book['title'] == search_term]
 
 
 def display_loan_totals():
-    pass
-
+    on_loan = sum(1 for book in library if book["isLoaned"])
+    not_on_loan = sum(1 for book in library if not book["isLoaned"])
+    print(f"Total books on loan: {on_loan}")
+    print(f"Total books not on loan: {not_on_loan}")
 
 def alter_book_status(book_title, new_status):
-    pass
+    for book in library:
+        if book["title"] == book_title:
+            book["isLoaned"] = new_status
+            break
 
 
 def add_new_book(library, title, author, is_loaned):
-    pass
+    new_book = {
+        "title": title,
+        "author": author,
+        "isLoaned": is_loaned
+    }
+    library.append(new_book)
 
 
 def remove_book(library, book_title):
-    pass
+    for i, book in enumerate(library):
+        if book["title"] == book_title:
+            del library[i]
+            return library
+    return library
 
 
 
